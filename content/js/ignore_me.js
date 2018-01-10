c4a101e24579b3ad03ef51e9b1c48f8de0e5916f
tonesto7 <tonesto7@gmail.com>
2018-01-09 20:35:33 -0500

    initial upload

---
+++ content/js/ignore_me.js
@@ -0,0 +1,398 @@
+'use esversion: 6';
+
+const ESStackName = 'EchoSistantV5';
+var functionArn = '';
+var echoSistantAPIURL = '';
+var repoId = '';
+var writableRepos = [];
+// var repoData = { owner: 'tonesto7', repoName: 'echosistant-alpha', branch: 'master', namespace: 'com.tonesto7'}
+const repoData = { owner: 'tonesto7', repoName: 'laundry-wizard', branch: 'master', namespace: 'tonesto7' };
+const appNames = { 'Laundry Wizard': 'smartapps/tonesto7/laundry-wizard.src/laundry-wizard.groovy' };
+var installedSmartapps;
+var retryCnt = 0;
+const authUrl = serverUrl + 'hub';
+const fetchReposUrl = serverUrl + 'github/writeableRepos';
+const updRepoUrl = serverUrl + 'githubAuth/updateRepos';
+const updFormUrl = serverUrl + 'githubAuth/updateForm';
+const doRepoUpdUrl = serverUrl + 'ide/app/doRepoUpdates';
+const smartappsListUrl = serverUrl + 'ide/apps';
+const availableSAs = serverUrl + '/api/smartapps/editable';
+
+function makeRequest(url, method, message, appId = null, appDesc = null, contentType = null, responseType = null, allow500 = false) {
+    return new Promise(function(resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        url += appId || '';
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    if (appId !== null && appDesc !== null) {
+                        // console.log(xhr.response);
+                        resolve({
+                            response: xhr.response,
+                            appId: appId,
+                            appDesc: appDesc
+                        });
+                    } else {
+                        resolve(xhr.response);
+                    }
+                } else if (xhr.status === 500 && allow500 === true) {
+                    resolve(xhr.response);
+                } else {
+                    reject(Error(xhr.statusText));
+                }
+            }
+        };
+        xhr.onprogress = function() {
+            // console.log('LOADING', xhr.readyState); // readyState will be 3
+        };
+        xhr.onerror = function() {
+            if (appId !== null && appDesc !== null) {
+                reject({
+                    statusText: xhr.statusText,
+                    appId: appId,
+                    appDesc: appDesc
+                });
+            } else {
+                reject(Error('XMLHttpRequest failed; error code:' + xhr.statusText));
+            }
+        };
+        xhr.open(method, url, true);
+        if (contentType !== null && responseType !== null) {
+            xhr.setRequestHeader('Content-Type', contentType);
+            xhr.responseType = responseType;
+            if (message) {
+                xhr.send(message);
+            } else {
+                xhr.send();
+            }
+        } else {
+            xhr.send(message);
+        }
+    });
+}
+
+function addResult(str, good) {
+    // $('#results').css({ display: 'none' });
+    $('#resultList').css({
+        display: 'block'
+    });
+    $('#resultsTitle').css({
+        display: 'block'
+    });
+    let s = "<li><span style='color: " + (good !== false ? '#25c225' : '#FF0000') + ";'>";
+    s += "<i class='fa fa-" + (good !== false ? 'check' : 'exclamation') + "'></i>";
+    s += '</span> ' + str + '</li>';
+    $('#resultList ul').append(s);
+}
+
+function installError(err, reload = true) {
+    if (reload) {
+        if (sessionStorage.refreshCount < 7) {
+            loaderFunc();
+        } else {
+            installComplete(err, true);
+        }
+    }
+}
+
+function installComplete(text, red = false) {
+    loaderDiv.style.display = 'none';
+    $('#loaderDiv').css({
+        display: 'none'
+    });
+    $('#finishedImg').removeClass('fa-exclamation-circle').addClass('fa-check').css({
+        display: 'block'
+    });
+    if (red) {
+        $('#finishedImg').removeClass('fa-check').addClass('fa-exclamation-circle').css({
+            color: 'red'
+        });
+    }
+    $('#results')
+        .css({
+            display: 'block'
+        })
+        .html(text + '<br/><br/>Press Back/Done Now');
+    sessionStorage.removeItem('appsDone');
+    sessionStorage.removeItem('refreshCount');
+}
+
+function buildRepoParamString(rdata) {
+    let objs = [];
+    objs.push('referringController=appIde');
+    objs.push('referringAction=apps');
+    objs.push('defaultNamespace=' + repoData.namespace);
+    objs.push('repos.id=0');
+    objs.push('repos.owner=' + repoData.namespace);
+    objs.push('repos.name=' + repoData.repoName);
+    objs.push('branch=' + repoData.branch);
+    for (let i in rdata) {
+        objs.push('repos.id=' + rdata[i].id);
+        if (rdata[i].owner !== undefined) {
+            objs.push('repos.owner=' + rdata[i].owner);
+        }
+        objs.push('repos.name=' + rdata[i].name);
+        objs.push('repos.branch=' + rdata[i].branch);
+    }
+    return objs.join('&');
+}
+
+function buildAppInstallParams(repoid, apps) {
+    let objs = [];
+    objs.push('id=' + repoid);
+    for (let i in apps) {
+        objs.push('added=' + apps[i]);
+    }
+    objs.push('publishUpdates=true');
+    objs.push('execute=Execute+Update');
+    return objs.join('&');
+}
+
+function processIntall() {
+    retryCnt++;
+    getStAuth()
+        .catch(function(err) {
+            installError(err, false);
+        })
+        .then(function(resp) {
+            if (resp === true) {
+                checkIdeForRepo(repoData.repoName, 'master')
+                    .catch(function(err) {
+                        installError(err, false);
+                    })
+                    .then(function(resp) {
+                        // console.log(resp);
+                        if (resp === false) {
+                            // addResult('', true);
+                            addRepoToIde(repoData.repoName, 'master')
+                                .catch(function(err) {
+                                    installError(err, false);
+                                })
+                                .then(function(resp) {
+                                    // console.log(resp);
+                                    checkIdeForRepo(repoData.repoName, 'master')
+                                        .catch(function(err) {
+                                            installError(err, false);
+                                        })
+                                        .then(function(resp) {
+                                            //   console.log(resp);
+                                            if (resp === true && appNames) {
+                                                checkIfAppsInstalled(appNames)
+                                                    .catch(function(err) {
+                                                        installError(err, false);
+                                                    })
+                                                    .then(function(resp) {
+                                                        // console.log('checkIfAppsInstalled: ', resp);
+                                                        if (Object.keys(resp).length) {
+                                                            // installComplete('Installs are Complete!<br/>Everything is Good!');
+                                                            installAppsToIde(resp)
+                                                                .catch(function(err) {
+                                                                    installError(err, false);
+                                                                })
+                                                                .then(function(resp) {
+                                                                    console.log('installAppsToIde: ', resp);
+                                                                    if (resp === true) {
+                                                                        installComplete('Installs are Complete!<br/>Everything is Good!');
+                                                                    }
+                                                                });
+                                                        } else {
+                                                            installComplete('Installs are Complete!<br/>Everything is Good!');
+                                                        }
+                                                    });
+                                            }
+                                        });
+                                });
+                        } else {
+                            addResult('Repo Exists: (' + repoData.repoName + ')', true);
+                            checkIfAppsInstalled(appNames)
+                                .catch(function(err) {
+                                    installError(err, false);
+                                })
+                                .then(function(resp) {
+                                    // console.log('checkIfAppsInstalled: ', resp);
+                                    if (Object.keys(resp).length) {
+                                        // installComplete('Installs are Complete!<br/>Everything is Good!');
+                                        installAppsToIde(resp)
+                                            .catch(function(err) {
+                                                installError(err, false);
+                                            })
+                                            .then(function(resp) {
+                                                // console.log('installAppsToIde: ', resp);
+                                                if (resp === true) {
+                                                    installComplete('Installs are Complete!<br/>Everything is Good!');
+                                                }
+                                            });
+                                    } else {
+                                        installComplete('Installs are Complete!<br/>Everything is Good!');
+                                    }
+                                });
+                        }
+                    });
+            } else {
+                if (retryCnt < 5) {
+                    processIntall();
+                } else {
+                    installComplete('Authentication Issue!<br/>Make Sure you Signed In!', true);
+                }
+            }
+        });
+}
+
+function getStAuth() {
+    return new Promise(function(resolve, reject) {
+        $('#loaderText1').text('Authenticating');
+        $('#loaderText2').text('Please Wait');
+        makeRequest(authUrl, 'GET', null)
+            .catch(function(err) {
+                installError(err);
+            })
+            .then(function(response) {
+                $('#results').text('');
+                addResult('SmartThings Authentication', true);
+                resolve(true);
+            });
+    });
+}
+
+function checkIdeForRepo(rname, branch) {
+    return new Promise(function(resolve, reject) {
+        let repoFound = false;
+        $('#loaderText1').text('Checking');
+        $('#loaderText2').text('Repos');
+        makeRequest(fetchReposUrl, 'GET', null)
+            .catch(function(err) {
+                installError(err, false);
+                addResult(err + ' Check Repo Issue', false);
+                reject(err);
+            })
+            .then(function(resp) {
+                // console.log(resp);
+                $('#loaderText1').text('Analyzing');
+                $('#loaderText2').text('Repos');
+                let respData = JSON.parse(resp);
+                writableRepos = respData;
+                if (respData.length) {
+                    for (let i in respData) {
+                        // console.log(respData[i]);
+                        if (respData[i].name === rname && respData[i].branch === branch) {
+                            repoId = respData[i].id;
+                            repoFound = true;
+                        }
+                    }
+                }
+                resolve(repoFound);
+            });
+    });
+}
+
+function checkIfAppsInstalled(apps) {
+    return new Promise(function(resolve, reject) {
+        // console.log('apps:', apps);
+        $('#loaderText1').text('Getting');
+        $('#loaderText2').text('Apps');
+        makeRequest(availableSAs, 'GET', null)
+            .catch(function(err) {
+                installError(err, false);
+                addResult(err + ' Getting SmartApps Issue', false);
+                reject(err);
+            })
+            .then(function(resp) {
+                // console.log(resp);
+                let fndApps = JSON.parse(resp);
+                if (fndApps.length) {
+                    $('#loaderText1').text('Analyzing');
+                    $('#loaderText2').text('Apps');
+
+                    for (let a in apps) {
+                        let fnd = false;
+                        for (let i in fndApps) {
+                            console.log('fndApp: ', fndApps[i].name, ' | requested app: ' + a);
+                            if (fndApps[i].name === a) {
+                                addResult(a + ' Exists Already', true);
+                                delete apps[a];
+                                break;
+                            }
+                        }
+                    }
+                }
+                resolve(apps);
+            });
+    });
+}
+
+function addRepoToIde(rname, branch) {
+    return new Promise(function(resolve, reject) {
+        let repoAdded = false;
+        $('#loaderText1').text('Adding');
+        $('#loaderText2').text('Repo to ST');
+        let repoParams = buildRepoParamString(writableRepos);
+        // console.log('repoParams: ', repoParams);
+        addResult('Repo Not Found - Adding to IDE', true);
+        makeRequest(updRepoUrl, 'POST', repoParams, null, null, 'application/x-www-form-urlencoded', '', true)
+            .catch(function(err) {
+                installError(err, false);
+                addResult(err + ' Add IDE Github Repo Issue', false);
+                installComplete('Error!<br/>Try Again Later!', true);
+                reject(err);
+            })
+            .then(function(resp) {
+                console.log(resp);
+                $('#loaderText1').text('Verifying');
+                $('#loaderText2').text('Repo');
+                checkIdeForRepo(rname, 'master')
+                    .catch(function(err) {
+                        installError(err, false);
+                        reject(err);
+                    })
+                    .then(function(resp) {
+                        if (resp === true) {
+                            addResult('Added Repo to IDE', true);
+                            addResult('Verified Repo Added', true);
+                        }
+                        resolve(resp);
+                    });
+                resolve(false);
+            });
+    });
+}
+
+function installAppsToIde(appNames) {
+    return new Promise(function(resolve, reject) {
+        $('#loaderText1').text('Beginning');
+        $('#loaderText2').text('Installs');
+        // console.log('repoParams: ', repoParams);
+        if (appNames) {
+            let repoParams = buildAppInstallParams(repoId, appNames);
+            makeRequest(doRepoUpdUrl, 'POST', repoParams, null, null, 'application/x-www-form-urlencoded', '', true)
+                .catch(function(err) {
+                    installError(err, false);
+                    addResult(err + ' Install Apps IDE Issue', false);
+                    installComplete('Error!<br/>Try Again Later!', true);
+                    reject(err);
+                })
+                .then(function(resp) {
+                    $('#loaderText1').text('Apps');
+                    $('#loaderText2').text('Installed');
+                    for (let i in appNames) {
+                        addResult(i + ' App Installed/Published', true);
+                    }
+                    resolve(true);
+                });
+        }
+    });
+}
+
+async function loaderFunc() {
+    if (functionType !== 'stackUtil') {
+        $('#results').text('Waiting for connection...');
+        if (sessionStorage.refreshCount === undefined) {
+            sessionStorage.refreshCount = '0';
+        }
+        sessionStorage.refreshCount = Number(sessionStorage.refreshCount) + 1;
+        await processIntall();
+    } else {
+        await lambdaUtil();
+    }
+}
+
+window.onload = loaderFunc;
---
+++ smartapps/tonesto7/st-installer.src/st-installer.groovy
@@ -0,0 +1,161 @@
+/*
+*   Universal Communtity App Installer
+*   Copyright 2018 Anthony Santilli, Corey Lista
+*
+// /**********************************************************************************************************************************************/
+import groovy.json.*
+import java.text.SimpleDateFormat
+
+definition(
+    name			: "Community-Installer",
+    namespace		: "tonesto7",
+    author			: "tonesto7/coreylista",
+    description		: "The Community Devices/SmartApp Installer",
+    category		: "My Apps",
+    singleInstance	: true,
+    iconUrl			: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url		: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX3Url		: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png")
+/**********************************************************************************************************************************************/
+private releaseVer() { return "5.0.0109" }
+private appVerDate() { "1-09-2018" }
+/**********************************************************************************************************************************************/
+preferences {
+    page name: "mainPage"
+}
+
+mappings {
+    path("/installStart") { action: [GET: "installStartHtml"] }
+}
+
+def mainPage() {
+    if(!atomicState?.accessToken) { atomicState?.accessToken = createAccessToken() }
+    dynamicPage (name: "mainPage", title: "", install: true, uninstall: true) {
+        def theURL = "https://consigliere-regional.api.smartthings.com/?redirect=${getAppEndpointUrl("installStart")}"
+        log.trace getAppEndpointUrl("stupdate")
+        section("Automatic Setup") {
+            href "", title: "Get Started", url: theURL, style: "embedded", required: false, description: "", image: ""
+        }
+    }
+}
+
+def webHeadHtml(title, verStr="") {
+    return """
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="${title}">
+        <meta name="author" content="Anthony S.">
+        <meta http-equiv="cleartype" content="on">
+        <meta name="MobileOptimized" content="320">
+        <meta name="HandheldFriendly" content="True">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <title>${title}</title>
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+        <script src="https://use.fontawesome.com/a81eef09c0.js" defer></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js" defer></script>
+        <link href="https://echosistant.com/es5_content/css/es5_master.min.css" rel="stylesheet">
+        <link href="https://echosistant.com/es5_content/css/es5_web.min.css" rel="stylesheet">
+    """
+}
+
+def webFooterHtml(verStr="") {
+    return """
+        <footer class="page-footer center-on-small-only fixed-bottom">
+            <div class="footer-copyright">
+                <div class="containter-fluid">
+                    © 2018 Copyright Anthony Santilli & Corey Lista
+                </div>
+            </div>
+        </footer>
+        <script type="text/javascript" src="https://echosistant.com/es5_content/js/popper.min.js" defer></script>
+        <script type="text/javascript" src="https://echosistant.com/es5_content/js/bootstrap.min.js" defer></script>
+        <script type="text/javascript" src="https://echosistant.com/es5_content/js/mdb.min.js" defer></script>
+    """
+}
+
+def getLoaderAnimation() {
+    return """
+        <svg id="loader" height="100%" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="lds-double-ring">
+            <circle cx="50" cy="50" ng-attr-r="{{config.radius}}" ng-attr-stroke-width="{{config.width}}" ng-attr-stroke="{{config.c1}}" ng-attr-stroke-dasharray="{{config.dasharray}}" fill="none" stroke-linecap="round" r="40" stroke-width="7" stroke="#18B9FF" stroke-dasharray="62.83185307179586 62.83185307179586" transform="rotate(139.357 50 50)">
+            <animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 50;360 50 50" keyTimes="0;1" dur="1.8s" begin="0s" repeatCount="indefinite"></animateTransform>
+            </circle>
+            <circle cx="50" cy="50" ng-attr-r="{{config.radius2}}" ng-attr-stroke-width="{{config.width}}" ng-attr-stroke="{{config.c2}}" ng-attr-stroke-dasharray="{{config.dasharray2}}" ng-attr-stroke-dashoffset="{{config.dashoffset2}}" fill="none" stroke-linecap="round" r="32" stroke-width="7" stroke="#FF7F27" stroke-dasharray="50.26548245743669 50.26548245743669" stroke-dashoffset="50.26548245743669" transform="rotate(-139.357 50 50)">
+            <animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 50;-360 50 50" keyTimes="0;1" dur="1.8s" begin="0s" repeatCount="indefinite"></animateTransform>
+            </circle>
+            <text id="loaderText1" fill="gray" stroke-width="0" x="50%" y="50%" text-anchor="middle" class="loaderText">Please</text>
+            <text id="loaderText2" fill="gray" stroke-width="0" x="50%" y="60%" text-anchor="middle" class="loaderText">Wait</text>
+        </svg>
+    """
+}
+
+def installStartHtml() {
+    def randVerStr = "?=${now()}"
+    def html = """
+        <html lang="en">
+        <head>
+            ${webHeadHtml("ST Community Install Tool", randVerStr)}
+            <script type="text/javascript">
+                var functionType = "addRepo";
+                var serverUrl = '${apiServerUrl('')}';
+                var locId = '${location?.id}'
+                // var repoData = ${new JsonOutput().toJson([owner: 'tonesto7', repoName: 'echosistant-alpha', branch: 'master', namespace: 'com.tonesto7'])};
+                // var appNames = ${new JsonOutput().toJson(['ST Home Manager':'smartapps/tonesto7/st-home-manager.src/st-home-manager.groovy', 'Event Reporting App':'smartapps/tonesto7/event-reporting-app.src/event-reporting-app.groovy'])};
+            </script>
+            <script src="https://echosistant.com/es5_content/js/ignore_me.js" async></script>
+        </head>
+        <body>
+            <header>
+                <nav class="navbar navbar-dark sticky-top navbar-expand-lg">
+                    <a class="navbar-brand" href="#"><img src="https://echosistant.com/es5_content/images/es5_logo.png" height="30" class="d-inline-block align-top" alt=""> Community Installer</a>
+                </nav>
+            </header>
+            <main>
+                <div class="container">
+                    <section id="stContent">
+                        <div style="width: 100%; height: 200px; text-align: center;">
+                            <h2 class="h2-responsive mb-2" style="font-weight: 400;">Software Installer</h2>
+                            <hr class="white">
+                            <div id="loaderDiv" class="row fadeIn fadeOut">
+                                <div class="col-lg-12 mb-r">
+                                    ${getLoaderAnimation()}
+                                </div>
+                            </div>
+                            <div class="row fadeIn fadeOut">
+                                <div class="col-lg-12 mb-r">
+                                    <div class="listDiv">
+                                        <div id="resultList">
+                                            <h3 id="resultsTitle" style="display: none;">Install Results</h3>
+                                            <ul id="resultUl"></ul>
+                                        </div>
+                                    </div>
+                                    <i id="finishedImg" class='fa fa-check' style="display: none;"></i>
+                                    <div id="results"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </main>
+            ${webFooterHtml(randVerStr)}
+        </body>
+        </html>"""
+    render contentType: "text/html", data: html
+}
+
+def getAccessToken() {
+    try {
+        if(!atomicState?.accessToken) {
+            LogAction("SmartThings Access Token Not Found... Creating a New One!!!","info", true)
+            atomicState?.accessToken = createAccessToken()
+        } else { return true }
+    }
+    catch (ex) {
+        log.error "Error: OAuth is not Enabled for ${app?.label}!.  Please click remove and Enable Oauth under the SmartApp App Settings in the IDE"
+        return false
+    }
+}
+
+def getAppImg(file)	    { return "https://echosistant.com/es5_content/images/$file" }
+def getAppVideo(file)	{ return "https://echosistant.com/es5_content/videos/$file" }
+def getAppEndpointUrl(subPath)	{ return "${apiServerUrl("/api/smartapps/installations/${app.id}${subPath ? "/${subPath}" : ""}?access_token=${atomicState.accessToken}")}" }
